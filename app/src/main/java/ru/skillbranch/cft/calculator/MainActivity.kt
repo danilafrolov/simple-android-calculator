713428b5b15cef4c22dfee56e1e3d45be2f05eaf
danila <frolovdanila1993@gmail.com>
2020-02-24 14:47:55 +0700

    initial version with layout

---
+++ app/src/main/java/ru/skillbranch/cft/calculator/MainActivity.kt
@@ -0,0 +1,16 @@
+package ru.skillbranch.cft.calculator
+
+import android.graphics.PorterDuff
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
