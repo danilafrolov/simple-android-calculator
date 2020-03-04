5398a7199b10902dde9c8b1a03b264ac81133e33
danila <frolovdanila1993@gmail.com>
2020-03-04 22:09:20 +0700

    Fixed test

--- app/src/androidTest/java/ru/cft/calculator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/cft/calculator/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("ru.skillbranch.cft.calculator", appContext.packageName)
+        assertEquals("ru.cft.calculator", appContext.packageName)
     }
 }
